029bb03d79505ef6f0d861ff85c070d9ba0f035a
Vodalok <visualcshape@gmail.com>
2017-02-25 00:53:58 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+BiliBili AD Skipper
+---
+
+#### Introduction
+ 
+ A *Chrome extension* can skip AD section on BiliBili.
+ 
+#### Notice
+ Support **HTML5 Player**
+ 
+ SWF supports coming soon.
+ 
+#### Instruction
+
+Please refer to [Load extension](https://developer.chrome.com/extensions/getstarted#unpacked)
+
---
+++ content.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Vodalok on 2017/2/24.
+ */
+
+(function(){
+    console.log("[BS] ...")
+
+    var videoDiv = $('div.scontent').get(0);
+
+    var videoDivObserver = new MutationObserver(function(mutations){
+        mutations.forEach(function(mutation){
+           for(var addedNodeIndex = 0; addedNodeIndex < mutation.addedNodes.length; addedNodeIndex++){
+               var aNode =  mutation.addedNodes[addedNodeIndex];
+               if(aNode.localName == 'iframe'){
+                   var replaceRegex = /pre_ad=\d+/;
+                   aNode.src = aNode.src.replace(replaceRegex, "pre_ad=0");
+                   break;
+               }
+           }
+        });
+    });
+    var videoDivCfg = {childList: true};
+    videoDivObserver.observe(videoDiv, videoDivCfg);
+}());
